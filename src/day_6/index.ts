f3c56fd103f0bf0a0c1d30fd4417b796eb25fca5
Quinton Maki <quintonmaki@gmail.com>
2022-12-05 21:28:45 -0800

    Day 6 complete

--- src/day_6/index.ts
+++ src/day_6/index.ts
@@ -1,21 +1,56 @@
-import * as url from 'url';
-import { parseData, submitAnswer } from '../utils.js'
-import { sendSolution } from '../api.js'
+import * as url from "url";
+import { parseData, submitAnswer } from "../utils.js";
+import { sendSolution } from "../api.js";
+
+enum Part {
+  One,
+  Two,
+}
+
+function uniqueString(part: Part, str: string): boolean {
+  const marker = part === Part.One ? 4 : 14;
+  if (str.length < marker) return false;
+  const letters = str
+    .split("")
+    .sort((a: string, b: string) => a.localeCompare(b));
+  for (let i = 1; i < letters.length; i++) {
+    if (letters[i] === letters[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 function solutionA() {
-    const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
-    const data = parseData(__dirname).split('\n')
+  const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+  const data = parseData(__dirname);
 
-    console.log(data)
-    //submitAnswer(__dirname, 1, data)
+  let buffer = "";
+
+  for (let i = 0; i < data.length; i++) {
+    buffer += data[i];
+
+    if (uniqueString(Part.One, buffer.slice(buffer.length - 4))) {
+      submitAnswer(__dirname, 1, i + 1);
+      return;
+    }
+  }
 }
 
 function solutionB() {
-    const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
-    const data = parseData(__dirname).split('\n')
+  const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+  const data = parseData(__dirname);
 
-    console.log(data)
-    //submitAnswer(__dirname, 2, data)
+  let buffer = "";
+
+  for (let i = 0; i < data.length; i++) {
+    buffer += data[i];
+
+    if (uniqueString(Part.Two, buffer.slice(buffer.length - 14))) {
+      submitAnswer(__dirname, 2, i + 1);
+      return;
+    }
+  }
 }
 
-solutionA()
+solutionB();

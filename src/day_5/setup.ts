5f19db7b15a216d5a502ac83898c5e3ee73cbd78
Quinton Maki <quintonmaki@gmail.com>
2022-12-05 20:42:47 -0800

    Remove days 26-31 from generation script

--- src/day_5/setup.ts
+++
@@ -1,9 +0,0 @@
-import path from "path";
-import * as url from "url";
-import { getInput } from "../api.js";
-import { getDay } from "../utils.js";
-
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-const day = getDay(__dirname);
-
-void getInput(2022, day, path.join(__dirname, "input.txt"));
